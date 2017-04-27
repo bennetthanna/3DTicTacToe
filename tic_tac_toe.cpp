f95eeec4d41e652e6816bb4a9e368f133c8c0c91
Hanna Bennett <bennetthanna22@gmail.com>
2017-04-27 13:56:07 -0700

    Checks for winner in on each 1D board TODO: Algorithm to check for winner through all boards in 3D space

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+bool winner = false;
+char winner_number = '0';
+
 void initialize_board (char board[4][4][4]) {
 	for (int i = 0; i < 4; ++i) {
 		for (int j = 0; j < 4; ++j) {
@@ -27,18 +30,62 @@ void print_board(char board[4][4][4]) {
 	}
 }
 
+void check_for_winner(char board[4][4][4]) {
+	for (int i = 0; i < 4; ++i) {
+		if (board[i][0][0] == board[i][1][1] && 
+			board[i][1][1] == board[i][2][2] && 
+			board[i][2][2] == board[i][3][3] &&
+			board[i][0][0] != 'N') {
+			winner_number = board[i][0][0];
+			winner = true;
+			cout << "The diagonal from top left to bottom right is equal" << endl;
+
+		} else if (board[i][0][3] == board[i][1][2] && 
+				   board[i][1][2] == board[i][2][1] && 
+				   board[i][2][1] == board[i][3][0] &&
+				   board[i][0][3] != 'N') {
+			winner_number = board[i][0][3];
+			winner = true;
+			cout << "The diagonal from bottom left to top right is equal" << endl;
+		}
+		for (int j = 0; j < 4; ++j) {
+			cout << board[i][j][0] << board[i][j][1] << board[i][j][2] << board[i][j][3] << endl;
+			if (board[i][j][0] == board[i][j][1] && 
+				board[i][j][1] == board[i][j][2] && 
+				board[i][j][2] == board[i][j][3] &&
+				board[i][j][0] != 'N') {
+				winner_number = board[i][j][0];
+				winner = true;
+				cout << "The row is equal and the winner is " << winner_number << endl;
+			} else if (board[i][0][j] == board[i][1][j] && 
+					   board[i][1][j] == board[i][2][j] && 
+					   board[i][2][j] == board[i][3][j] &&
+					   board[i][0][j] != 'N') {
+				winner_number = board[i][0][j];
+				winner = true;
+				cout << "The column is equal and the winner is " << winner_number << endl;
+			}
+		}
+	}
+}
+
 // cases where someone wins on board[i][j][k]
 // on same board (all i's are the same)
-	// all rows are the same (all j's are the same)
-	// all columns are the same (all k's are the same)
-	// diagonal (0,0)(1,1)(2,2)(3,3) or (3,0)(2,1)(1,2)(0,3)
+	// all rows are the same (all j's are the same) DONE
+	// all columns are the same (all k's are the same) DONE
+	// diagonal (0,0)(1,1)(2,2)(3,3) or (3,0)(2,1)(1,2)(0,3) DONE
 // on all different boards (all i's are different)
 	// all same coordinate (j and k same - example: all (0,0) on all boards - board[i][j][k] == board[i+1][j][k])
 	// diagonal down the boards
+		// all j's are the same, all k's are different
+		// all k's are the same, all j's are different
+		// (0,0,0)(1,1,1)(2,2,2)(3,3,3)
+		// (0,0,3)(1,1,2)(2,2,1)(3,3,0)
+		// (0,3,0)(1,2,1)(2,1,2)(3,0,3)
+		// (0,3,3)(1,2,2)(2,1,1)(3,0,0)
 
 int main() {
 
-	bool winner = false;
 	int board_number, row_number, column_number, player;
 
 	cout << endl << "Welcome to 3D Four Board Tic Tac Toe" << endl;
@@ -101,6 +148,7 @@ int main() {
 		}
 
 		// check if someone won
+		check_for_winner(board);
 
 	} while (!winner);
 }
