8efd3ff929a5f31b7b422fdd90381ae537e7d59a
Hanna Bennett <bennetthanna22@gmail.com>
2017-05-01 15:06:59 -0700

    Implemented all ways to check for winners

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -31,6 +31,36 @@ void print_board(char board[4][4][4]) {
 }
 
 void check_for_winner(char board[4][4][4]) {
+	if (board[0][0][0] == board[1][1][1] && 
+		board[1][1][1] == board[2][2][2] && 
+		board[2][2][2] == board[3][3][3] &&
+		board[0][0][0] != 'N') {
+		winner_number = board[0][0][0];
+		winner = true;
+		cout << "The diagonal through from top left back to bottom right front" << endl;
+	} else if (board[0][0][3] == board[1][1][2] && 
+			   board[1][1][2] == board[2][2][1] && 
+   			   board[2][2][1] == board[3][3][0] &&
+   			   board[0][0][3] != 'N') {
+		winner_number = board[0][0][3];
+		winner = true;
+		cout << "The diagonal through from top right back to bottom left front" << endl;
+	} else if (board[0][3][0] == board[1][2][1] && 
+			   board[1][2][1] == board[2][1][2] && 
+		   	   board[2][1][2] == board[3][0][3] &&
+		   	   board[0][3][0] != 'N') {
+		winner_number = board[0][3][0];
+		winner = true;
+		cout << "The diagonal through from top left front to bottom right back" << endl;
+	} else if (board[0][3][3] == board[1][2][2] && 
+			   board[1][2][2] == board[2][1][1] && 
+			   board[2][1][1] == board[3][0][0] &&
+		 	   board[0][3][3] != 'N') {
+		winner_number = board[0][3][3];
+		winner = true;
+		cout << "The diagonal through from top right front to bottom left back" << endl;
+	}
+
 	for (int i = 0; i < 4; ++i) {
 		if (board[i][0][0] == board[i][1][1] && 
 			board[i][1][1] == board[i][2][2] && 
@@ -39,7 +69,6 @@ void check_for_winner(char board[4][4][4]) {
 			winner_number = board[i][0][0];
 			winner = true;
 			cout << "The diagonal from top left to bottom right is equal" << endl;
-
 		} else if (board[i][0][3] == board[i][1][2] && 
 				   board[i][1][2] == board[i][2][1] && 
 				   board[i][2][1] == board[i][3][0] &&
@@ -47,9 +76,36 @@ void check_for_winner(char board[4][4][4]) {
 			winner_number = board[i][0][3];
 			winner = true;
 			cout << "The diagonal from bottom left to top right is equal" << endl;
+		} else if (board[0][i][0] == board[1][i][1] && 
+				   board[1][i][1] == board[2][i][2] && 
+				   board[2][i][2] == board[3][i][3] &&
+				   board[0][i][0] != 'N') {
+			winner_number = board[0][i][0];
+			winner = true;
+			cout << "The diagonal through from top left to bottom right same row" << endl;
+		} else if (board[0][i][3] == board[1][i][2] && 
+				   board[1][i][2] == board[2][i][1] && 
+				   board[2][i][1] == board[3][i][0] &&
+				   board[0][i][3] != 'N') {
+			winner_number = board[0][i][3];
+			winner = true;
+			cout << "The diagonal through from top right to bottom left same row" << endl;
+		} else if (board[0][3][i] == board[1][2][i] && 
+				   board[1][2][i] == board[2][1][i] && 
+				   board[2][1][i] == board[3][0][i] &&
+				   board[0][3][i] != 'N') {
+			winner_number = board[0][i][0];
+			winner = true;
+			cout << "The diagonal through from top front to bottom back same column" << endl;
+		} else if (board[0][0][i] == board[1][1][i] && 
+				   board[1][1][i] == board[2][2][i] && 
+				   board[2][2][i] == board[3][3][i] &&
+				   board[0][0][i] != 'N') {
+			winner_number = board[0][0][i];
+			winner = true;
+			cout << "The diagonal through from top back to bottom front same column" << endl;
 		}
 		for (int j = 0; j < 4; ++j) {
-			cout << board[i][j][0] << board[i][j][1] << board[i][j][2] << board[i][j][3] << endl;
 			if (board[i][j][0] == board[i][j][1] && 
 				board[i][j][1] == board[i][j][2] && 
 				board[i][j][2] == board[i][j][3] &&
@@ -64,25 +120,35 @@ void check_for_winner(char board[4][4][4]) {
 				winner_number = board[i][0][j];
 				winner = true;
 				cout << "The column is equal and the winner is " << winner_number << endl;
+			} else if (board[0][i][j] == board[1][i][j] &&
+					   board[1][i][j] == board[2][i][j] &&
+					   board[2][i][j] == board[3][i][j] &&
+					   board[0][i][j] != 'N') {
+				winner_number = board[0][i][j];
+				winner = true;
+				cout << "The depth columns are the same" << endl;
 			}
 		}
 	}
 }
 
 // cases where someone wins on board[i][j][k]
+// i = board #
+// j = row #
+// k = column #
 // on same board (all i's are the same)
 	// all rows are the same (all j's are the same) DONE
 	// all columns are the same (all k's are the same) DONE
 	// diagonal (0,0)(1,1)(2,2)(3,3) or (3,0)(2,1)(1,2)(0,3) DONE
 // on all different boards (all i's are different)
-	// all same coordinate (j and k same - example: all (0,0) on all boards - board[i][j][k] == board[i+1][j][k])
+	// all same coordinate (j and k same - example: all (0,0) on all boards - board[i][j][k] == board[i+1][j][k]) DONE
 	// diagonal down the boards
-		// all j's are the same, all k's are different
-		// all k's are the same, all j's are different
-		// (0,0,0)(1,1,1)(2,2,2)(3,3,3)
-		// (0,0,3)(1,1,2)(2,2,1)(3,3,0)
-		// (0,3,0)(1,2,1)(2,1,2)(3,0,3)
-		// (0,3,3)(1,2,2)(2,1,1)(3,0,0)
+		// all j's are the same, all k's are different DONE
+		// all k's are the same, all j's are different DONE
+		// (0,0,0)(1,1,1)(2,2,2)(3,3,3) DONE
+		// (0,0,3)(1,1,2)(2,2,1)(3,3,0) DONE
+		// (0,3,0)(1,2,1)(2,1,2)(3,0,3) DONE
+		// (0,3,3)(1,2,2)(2,1,1)(3,0,0) DONE
 
 int main() {
 
@@ -93,7 +159,7 @@ int main() {
 	cout << "You will do so in the format board_number row_number column_number" << endl;
 	cout << "Options range from 0-3" << endl;
 	cout << "For example: 0 3 1" << endl;
-	cout << "This would be board 0, row 3, column 1" << endl << endl;
+	cout << "This would be board 0, row 3, column 1 and would be as follows" << endl << endl;
 
 	char board[4][4][4];
 	initialize_board(board);
