961137a95c6897d48213c168c348390baf847233
Hanna Bennett <bennetthanna22@gmail.com>
2017-05-01 19:24:25 -0700

    Added comments. Final working product

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -8,6 +8,7 @@ bool winner = false;
 char winner_number = '0';
 string winning_move;
 
+// initialize the board to all empty squares
 void initialize_board (char board[4][4][4]) {
 	for (int i = 0; i < 4; ++i) {
 		for (int j = 0; j < 4; ++j) {
@@ -18,6 +19,7 @@ void initialize_board (char board[4][4][4]) {
 	}
 }
 
+// print out the board with the board numbers, the row numbers, and the column numbers
 void print_board(char board[4][4][4]) {
 	for (int i = 0; i < 4; ++i) {
 		cout << "Board " << i << endl;
@@ -30,6 +32,25 @@ void print_board(char board[4][4][4]) {
 		} cout << endl;
 	}
 }
+
+// check for winning cases
+// cases where someone wins on board[i][j][k]
+// i = board #
+// j = row #
+// k = column #
+// on same board (i's are the same)
+	// across a row (j's are the same, k's are different)
+	// down a column (k's are the same, j's are different)
+	// diagonal left top to right bottom (i,0,0)(i,1,1)(i,2,2)(i,3,3)
+	// diagonal right top to left bottom (i,3,0)(i,2,1)(i,1,2)(i,0,3)
+// on all different boards (i's are different)
+	// same coordinate through the boards (j's and k's are the same - example: (1,2) for all i's)
+	// diagonal through the boards in same row (j's are the same, k's are different)
+	// diagonal through the boards in same column (k's are the same, j's are different)
+	// diagonal through the boards top left back to bottom right front (0,0,0)(1,1,1)(2,2,2)(3,3,3)
+	// diagonal through the boards top right back to bottom left front (0,0,3)(1,1,2)(2,2,1)(3,3,0)
+	// diagonal through the boards bottom left front to top right back (0,3,0)(1,2,1)(2,1,2)(3,0,3)
+	// diagonal through the boards bottom right front to top left back (0,3,3)(1,2,2)(2,1,1)(3,0,0)
 
 void check_for_winner(char board[4][4][4]) {
 	if (board[0][0][0] == board[1][1][1] && 
@@ -133,28 +154,13 @@ void check_for_winner(char board[4][4][4]) {
 	}
 }
 
-// cases where someone wins on board[i][j][k]
-// i = board #
-// j = row #
-// k = column #
-// on same board (all i's are the same)
-	// all rows are the same (all j's are the same) DONE
-	// all columns are the same (all k's are the same) DONE
-	// diagonal (0,0)(1,1)(2,2)(3,3) or (3,0)(2,1)(1,2)(0,3) DONE
-// on all different boards (all i's are different)
-	// all same coordinate (j and k same - example: all (0,0) on all boards - board[i][j][k] == board[i+1][j][k]) DONE
-	// diagonal down the boards
-		// all j's are the same, all k's are different DONE
-		// all k's are the same, all j's are different DONE
-		// (0,0,0)(1,1,1)(2,2,2)(3,3,3) DONE
-		// (0,0,3)(1,1,2)(2,2,1)(3,3,0) DONE
-		// (0,3,0)(1,2,1)(2,1,2)(3,0,3) DONE
-		// (0,3,3)(1,2,2)(2,1,1)(3,0,0) DONE
-
 int main() {
 
 	int board_number, row_number, column_number, player;
+	char board[4][4][4];
+	initialize_board(board);
 
+	// explain the background of the game and give a visual example of a valid move
 	cout << endl << "Welcome to 3D Four Board Tic Tac Toe" << endl;
 	cout << "When it is your turn, you will be prompted to enter your desired move" << endl;
 	cout << "You will do so in the format board_number row_number column_number" << endl;
@@ -162,17 +168,19 @@ int main() {
 	cout << "For example: 0 3 1" << endl;
 	cout << "This would be board 0, row 3, column 1 and would be as follows" << endl << endl;
 
-	char board[4][4][4];
-	initialize_board(board);
 	board[0][3][1] = 'X';
 	print_board(board);
+
+	// reset board
 	board[0][3][1] = ' ';
 	player = 1;
 
+	// continuously prompt players while there is no winner
 	do {
 		cout << "Player " << player << " what's your move?\n";
 		cin >> board_number >> row_number >> column_number;
 		cout << endl;
+		// error check user input
 		while (board_number < 0 || board_number > 3 || 
 			   row_number < 0 || row_number > 3 ||
 			   column_number < 0 || column_number > 3 ||
@@ -187,9 +195,11 @@ int main() {
 				cin >> board_number >> row_number >> column_number;
 			}
 		}
+		// if valid move, set chosen square to player's number
 		board[board_number][row_number][column_number] = player+'0';
 		print_board(board);
 
+		// toggle players
 		if (player == 1) {
 			player = 2;
 		} else {
@@ -200,6 +210,7 @@ int main() {
 
 	} while (!winner);
 
+	// output who the winner is and how they won
 	cout << "Congratulations Player " << winner_number << "! You're the winner!" << endl;
 	cout << "The winning move was " << winning_move << endl << endl;
 }
